060ee9367728677aa78a13987ceb91f92357a6e9
AndreiKhaustov <cryptomonkey1994@gmail.com>
2024-06-12 22:49:51 +0300

    Added order-execution project

---
+++ src/main/java/com/order/execution/order_execution/OrderExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.order.execution.order_execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderExecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderExecutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order/execution/order_execution/OrderExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.execution.order_execution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
