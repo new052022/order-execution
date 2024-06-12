ef29acc362a29df66e145519eda1c9a698048f7f
AndreiKhaustov <cryptomonkey1994@gmail.com>
2024-06-12 23:25:24 +0300

    Added order-execution project

---
+++ src/main/java/com/order/execution/order_execution/service/impl/OrderStoreServiceImpl.java
@@ -0,0 +1,10 @@
+package com.order.execution.order_execution.service.impl;
+
+import com.order.execution.order_execution.service.interfaces.OrderStoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderStoreServiceImpl implements OrderStoreService {
+}
---
+++ src/main/java/com/order/execution/order_execution/service/interfaces/OrderStoreService.java
@@ -0,0 +1,4 @@
+package com.order.execution.order_execution.service.interfaces;
+
+public interface OrderStoreService {
+}
